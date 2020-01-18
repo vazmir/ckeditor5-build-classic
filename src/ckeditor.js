c46859d6c7000d99a800c04ac7c13eab94e15226
vazmir <vazquez.miriam@gamial.com>
2020-01-18 20:23:06 +0100

    colo

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -20,12 +20,12 @@ import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
 import Indent from '@ckeditor/ckeditor5-indent/src/indent';
 import Link from '@ckeditor/ckeditor5-link/src/link';
 import List from '@ckeditor/ckeditor5-list/src/list';
-import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import Base64UploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/base64uploadadapter';
+import FontColor from '@ckeditor/ckeditor5-font/src/fontcolor'
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
@@ -45,12 +45,12 @@ ClassicEditor.builtinPlugins = [
 	Indent,
 	Link,
 	List,
-	MediaEmbed,
 	Paragraph,
 	PasteFromOffice,
 	Table,
 	TableToolbar,
-	Base64UploadAdapter
+	Base64UploadAdapter,
+	FontColor
 ];
 
 // Editor configuration.
@@ -61,6 +61,8 @@ ClassicEditor.defaultConfig = {
 			'|',
 			'bold',
 			'italic',
+			'fontColor',
+			'|',
 			'link',
 			'bulletedList',
 			'numberedList',
@@ -71,7 +73,6 @@ ClassicEditor.defaultConfig = {
 			'imageUpload',
 			'blockQuote',
 			'insertTable',
-			'mediaEmbed',
 			'undo',
 			'redo'
 		]
@@ -90,6 +91,83 @@ ClassicEditor.defaultConfig = {
 			'tableRow',
 			'mergeTableCells'
 		]
+	},
+	fontColor:{
+		colors: [
+			{
+				color: 'hsl(0, 0%, 0%)',
+				label: 'Black'
+			},
+			{
+				color: 'hsl(295, 20%, 12%)',
+				label: 'Dark'
+			},
+			{
+				color: 'hsl(0, 0%, 30%)',
+				label: 'Dim grey'
+			},
+			{
+				color: 'hsl(0, 0%, 60%)',
+				label: 'Grey'
+			},
+			{
+				color: 'hsl(0, 0%, 90%)',
+				label: 'Light grey'
+			},
+			{
+				color: 'hsl(0, 75%, 60%)',
+				label: 'Red'
+			},
+			{
+				color: 'hsl(30, 75%, 60%)',
+				label: 'Orange'
+			},
+			{
+				color: 'hsl(60, 75%, 60%)',
+				label: 'Yellow'
+			},
+			{
+				color: 'hsl(38, 71%, 52%)',
+				label: 'Warning'
+			},
+			{
+				color: 'hsl(90, 75%, 60%)',
+				label: 'Light green'
+			},
+			{
+				color: 'hsl(120, 75%, 60%)',
+				label: 'Green'
+			},
+			{
+				color: 'hsl(150, 75%, 60%)',
+				label: 'Aquamarine'
+			},
+			{
+				color: 'hsl(180, 75%, 60%)',
+				label: 'Turquoise'
+			},
+			{
+				color: 'hsl(210, 75%, 60%)',
+				label: 'Light blue'
+			},
+			{
+				color: 'hsl(240, 75%, 60%)',
+				label: 'Blue'
+			},
+			{
+				color: 'hsl(278, 75%, 52%)',
+				label: 'Purple'
+			},
+			{
+				color: 'hsl(205, 23%, 56%)',
+				label: 'Grey blue'
+			},
+			{
+				color: 'hsl(347, 51%, 48%)',
+				label: 'Burdeos'
+			}
+		],
+		columns: 6
 	},
 	// This value must be kept in sync with the language defined in webpack.config.js.
 	language: 'en'
