70790565d25c7d8025b7616fcf918a67359fe790
vazmir <vazquez.miriam@gamial.com>
2020-05-02 15:05:49 +0200

    less colors

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -120,48 +120,24 @@ ClassicEditor.defaultConfig = {
 				label: 'Dark'
 			},
 			{
-				color: 'hsl(0, 0%, 30%)',
-				label: 'Dim grey'
-			},
-			{
 				color: 'hsl(0, 0%, 60%)',
 				label: 'Grey'
-			},
-			{
-				color: 'hsl(0, 0%, 90%)',
-				label: 'Light grey'
 			},
 			{
 				color: 'hsl(0, 75%, 60%)',
 				label: 'Red'
 			},
 			{
-				color: 'hsl(30, 75%, 60%)',
-				label: 'Orange'
-			},
-			{
-				color: 'hsl(60, 75%, 60%)',
-				label: 'Yellow'
-			},
-			{
 				color: 'hsl(38, 71%, 52%)',
 				label: 'Warning'
-			},
-			{
-				color: 'hsl(90, 75%, 60%)',
-				label: 'Light green'
 			},
 			{
 				color: 'hsl(120, 75%, 60%)',
 				label: 'Green'
 			},
 			{
-				color: 'hsl(150, 75%, 60%)',
-				label: 'Aquamarine'
-			},
-			{
-				color: 'hsl(180, 75%, 60%)',
-				label: 'Turquoise'
+				color: 'hsl(126, 31%, 52%)',
+				label: 'Success'
 			},
 			{
 				color: 'hsl(210, 75%, 60%)',
